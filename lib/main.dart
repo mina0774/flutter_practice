7f2fdefb32c0bf3080cdb82292c63eac25dae619
mina <mina0774@hanmail.net>
2020-06-12 15:04:17 +0900

    add interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,10 @@ class MyApp extends StatelessWidget {
 
 class RandomWordState extends State<RandomWords>{
   // Dart 언어 앞에 _를 붙이면 private 적용
-  final _suggestions=<WordPair>[];
-  final _biggerFont=const TextStyle(fontSize: 18.0);
+  final List<WordPair>_suggestions=<WordPair>[];
+  final Set<WordPair> _saved=Set<WordPair>();
+  final TextStyle _biggerFont= TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -43,11 +45,25 @@ class RandomWordState extends State<RandomWords>{
   }
 
   Widget _buildRow(WordPair pair){
+    final bool alreadySaved=_saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
