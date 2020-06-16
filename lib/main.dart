50571a04304773fb3e88b34c599d6c34c55bf3fe
mina <mina0774@hanmail.net>
2020-06-16 16:53:40 +0900

    navigate to a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,43 @@ class RandomWordState extends State<RandomWords>{
     return Scaffold(
       appBar: AppBar(
         title: Text('StartUp Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list),onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile
+              .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+              .toList();
+
+          return Scaffold(         // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );                       // ... to here.
+        },
+      ),
     );
   }
 
